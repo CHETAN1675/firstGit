f1b1fc8decd971e2233026adc49d02c1bbb3c8c1
CHETAN1675 <mahajanchetan001@gmail.com>
2025-07-11 19:09:19 +0530

    updated title

---
+++ Task 1/fruitworld.js
@@ -0,0 +1,15 @@
+// Write your code below:
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit basket";
+mainHeading.style.color = "orange";
+
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+const basketheading = document.getElementById("basket-heading");
+basketheading.style.color = "green"
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = '<p>Please visit us again</p>'
+
