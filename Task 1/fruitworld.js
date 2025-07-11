6d4de7ed6e06bedf7422412e19391a5256cf02ab
CHETAN1675 <mahajanchetan001@gmail.com>
2025-07-12 00:09:10 +0530

    4th element color changed to blue and fontstyle updated to italic

--- Task 1/fruitworld.js
+++ Task 1/fruitworld.js
@@ -13,9 +13,10 @@ basketheading.style.color = "green"
 const thanks = document.getElementById("thanks");
 thanks.innerHTML = '<p>Please visit us again</p>'
 
-const fruit = document.getElementsByClassName("fruit");
-fruit[2].style.backgroundColor = 'yellow'
+const listItems = document.getElementsByTagName("li");
+listItems[4].style.color = "blue"
 
-for (let i = 0; i < fruit.length; i++){
-    fruit[i].style.fontWeight = "bold";
+for (let i = 0; i < listItems.length; i++){
+    listItems[i].style.fontStyle = "italic"
 }
+
